fe35d93e2a873d921e6c661c4bd27499d26cc076
Hassan Saab <hassansaab@hassans-mbp-2.mynetworksettings.com>
2024-01-29 20:47:59 -0500

    making change to app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and some other cool stuff...dun..dun..dunn..
+          Learn React and some other cool stuff
         </a>
       </header>
     </div>
