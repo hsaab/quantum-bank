32bf97ffb877e1a775978a8f240a1bf83cb75bb5
Hassan Saab <hassansaab@hassans-mbp-2.mynetworksettings.com>
2024-01-30 15:35:47 -0500

    updating app page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and some other cool stuff
+          Learn React and some other cool stuff...dun...dun...dun
         </a>
       </header>
     </div>
