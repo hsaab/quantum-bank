6d00cb742cfa28d8892b9c789717cd4c6a98c55b
hassan <hassan.saab@harness.io>
2024-03-13 13:10:17 -0400

    updating test dockerfile

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Updating....
+          Updating....updating...
         </a>
       </header>
     </div>
