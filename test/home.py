04b17bfd667676465eac38de5537480a52541a27
hassan <hassan.saab@harness.io>
2024-05-16 14:56:40 -0400

    update tes

--- test/home.py
+++ test/home.py
@@ -12,4 +12,4 @@ def test_home_status_code(client):
 
 def test_home_template(client):
     response = client.get('/')
-    assert b"This is Massively" in response.data
+    assert b"This is HARNESS" in response.data
