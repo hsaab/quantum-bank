893d0bfdd28be4e37878f539d151bbb285d00722
hassan <hassan.saab@harness.io>
2024-05-16 14:18:07 -0400

    fix server and time

--- api/time.py
+++ api/time.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 def handle_time():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return "Current server time is: {current_time}"
+    return f"Current server time is: {current_time}"
--- server.py
+++ server.py
@@ -28,4 +28,4 @@ def page_not_found(e):
     return handle_404()
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(host='0.0.0.0', port=8000)
