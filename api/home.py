cefb5774ff8d59173b2a21846308e68ccc684789
hassan <hassan.saab@harness.io>
2024-05-31 11:19:00 -0400

    add TI

--- api/home.py
+++ api/home.py
@@ -1,5 +1,5 @@
 from flask import render_template 
 
 def handle_home():
+    # adding comment to trigger tests for home
     return render_template('new_home.html')
-    # return "Welcome to the Home Page!"
